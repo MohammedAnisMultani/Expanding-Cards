d89ad03166d4539976c375b1449a940f51c2777e
MohammedAnis <mohammedanismultani@gmail.com>
2024-03-12 18:34:14 +0530

    expanding card

---
+++ script.js
@@ -0,0 +1,15 @@
+let panel = document.querySelectorAll(".panel")
+panel.forEach(panel=>{
+  panel.addEventListener("click",(e)=>{
+    removing();
+    panel.classList.add("active")
+  })
+})
+
+function removing(){
+  panel.forEach(panel=>{
+    panel.classList.remove("active");
+  })
+}
+
+// ---------------------------------
